06858e41acf4c1aeaa7b6d8bd94f17ade78c0681
kitsanapol-Kaspia <kitsanapol.saetan@gmail.com>
2026-03-17 02:25:35 +0000

    Initial commit

---
+++ data-processing.py
@@ -0,0 +1,20 @@
+## cleaning data
+print("Data Processing Steps")
+
+## write first function
+
+## write second function
+
+import pandas as pd
+df = pd.read_csv(..)
+
+result = fun1(df)
+
+final_result = fun2(result)
+
+
+def greeting()
+    print("Hi Kit")
+
+  greeting()
+
